40629635f868f34f9ddf048624fa17084da6b428
PipeVerach <felipe@kuantica.cl>
2024-09-11 01:06:51 -0300

    second

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 const items = [
-  'https://picsum.photos/1920/1080?random=1',
-  'https://picsum.photos/1920/1080?random=2',
-  'https://picsum.photos/1920/1080?random=3',
-  'https://picsum.photos/1920/1080?random=4',
-  'https://picsum.photos/1920/1080?random=5',
-  'https://picsum.photos/1920/1080?random=6'
+  {
+    img: 'https://picsum.photos/1920/1080?random=1',
+    text: 'Texto para imagen 1'
+  },
+  {
+    img: 'https://picsum.photos/1920/1080?random=2',
+    text: 'Texto para imagen 2'
+  },
+  {
+    img: 'https://picsum.photos/1920/1080?random=3',
+    text: 'Texto para imagen 3'
+  },
+  {
+    img: 'https://picsum.photos/1920/1080?random=4',
+    text: 'Texto para imagen 4'
+  },
+  {
+    img: 'https://picsum.photos/1920/1080?random=5',
+    text: 'Texto para imagen 5'
+  },
+  {
+    img: 'https://picsum.photos/1920/1080?random=6',
+    text: 'Texto para imagen 6'
+  }
 ]
 
 const carouselRef = ref()
@@ -19,7 +37,7 @@ onMounted(() => {
     }
 
     carouselRef.value.next()
-  }, 3000)
+  }, 5000)
 })
 </script>
 
@@ -29,10 +47,17 @@ onMounted(() => {
     v-slot="{ item }"
     :items="items"
     :ui="{ item: 'basis-full' }"
-    class="max-h-[600px] overflow-hidden"
+    class="max-h-[600px] overflow-hidden relative"
     indicators
   >
-    <img :src="item" class="w-full h-full" draggable="false">
+    <!-- Contenedor de la imagen y el texto superpuesto -->
+    <div class="relative w-full h-full flex items-center justify-center">
+      <img :src="item.img" class="w-full h-full object-cover" draggable="false">
+
+      <!-- Texto superpuesto -->
+      <div class="absolute  w-full p-4 text-white text-2xl text-center font-bold">
+        {{ item.text }}
+      </div>
+    </div>
   </UCarousel>
 </template>
-
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <div >
     <Navbar />
     <Carousel />
+    <div class="w-full h-full flex flex-col justify-center items-center mt-10" >
+      <h1 class="text-5xl" >Javi <3 </h1>
+        <h2 class="text-4xl">uwu</h2>
+    </div>
   </div>
 </template>
 
