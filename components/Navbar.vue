2c5ed139e6b27ec6a2bbf40b76530c16075b1f6c
PipeVerach <felipe@kuantica.cl>
2024-09-11 01:12:34 -0300

    text-white

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,10 +2,10 @@
   <nav class="p-6 bg-purple-800">
     <div class="flex justify-between items-end max-w-[1300px] mx-auto">
       <div>
-        <p class="text-5xl">Fvera</p>
+        <p class="text-5xl text-white">Fvera</p>
       </div>
       <div class="md:flex hidden space-x-6">
-        <a :href="item.path" :title="item.name" class="text-2xl" v-for="(item, index) in navItems" :key="index">{{ item.name }}</a>
+        <a :href="item.path" :title="item.name" class="text-2xl text-white" v-for="(item, index) in navItems" :key="index">{{ item.name }}</a>
       </div>
       <div class="md:hidden block">
         <button @click="toggleMenu">
@@ -16,7 +16,7 @@
     <div :class="{ 'max-h-96': isMenuOpen, 'max-h-0': !isMenuOpen }" class="overflow-hidden absolute z-50 top-[84px] left-0 w-full bg-purple-800 md:hidden transition-all duration-500 ease-in-out">
       <ul class="flex flex-col font-normal text-sm font-mont gap-2 p-4">
         <li v-for="item in navItems" :key="item.url">
-          <a :href="item.path" class="block py-2 px-2 text-2xl">
+          <a :href="item.path" class="block py-2 px-2 text-2xl text-white">
             {{ item.name }}
           </a>
         </li>
