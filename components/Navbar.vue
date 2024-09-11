531505f2b35d7a0826711771c51f83b4b7e9f487
PipeVerach <felipe@kuantica.cl>
2024-09-11 01:00:52 -0300

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="p-6 bg-purple-800">
+    <div class="flex justify-between items-end max-w-[1300px] mx-auto">
+      <div>
+        <p class="text-5xl">Fvera</p>
+      </div>
+      <div class="md:flex hidden space-x-6">
+        <a :href="item.path" :title="item.name" class="text-2xl" v-for="(item, index) in navItems" :key="index">{{ item.name }}</a>
+      </div>
+      <div class="md:hidden block">
+        <button @click="toggleMenu">
+          <img src="/images/icons/bars.svg" alt="bars" title="bars" class="w-8">
+        </button>
+      </div>
+    </div>
+    <div :class="{ 'max-h-96': isMenuOpen, 'max-h-0': !isMenuOpen }" class="overflow-hidden absolute top-[84px] left-0 w-full bg-purple-800 md:hidden transition-all duration-500 ease-in-out">
+      <ul class="flex flex-col font-normal text-sm font-mont gap-2 p-4">
+        <li v-for="item in navItems" :key="item.url">
+          <a :href="item.path" class="block py-2 px-2 text-2xl">
+            {{ item.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuOpen: false, // Controla si el menú está abierto o cerrado
+      navItems: [
+        {
+          name: "Inicio",
+          path: "/"
+        },
+        {
+          name: "Paginas web",
+          path: "/web"
+        },
+        {
+          name: "Proyectos",
+          path: "/proyectos"
+        }
+      ]
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen; // Alterna la visibilidad del menú
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div >
+    <Navbar />
+    <Carousel />
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
