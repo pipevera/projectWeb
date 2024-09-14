f60506c03b1b63c920dd64c0d75c5bf0f7dd9c9d
PipeVerach <felipe@kuantica.cl>
2024-09-14 13:27:56 -0300

    whatsapp fixed

--- components/Description.vue
+++ components/Description.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="max-w-[1300px] mx-auto py-20" >
-    <div class="flex">
-      <div class="w-full md:w-2/3 space-y-6 p-6" >
-        <h2 class="text-5xl" >Lorem ipsum dolor sit amet consectetur</h2>
+    <div class="flex flex-col md:flex md:flex-row">
+      <div class="w-full md:w-2/3 space-y-6 p-6 animate-fade-right" >
+        <h2 class="text-5xl text-balance" >Lorem ipsum dolor sit amet consectetur</h2>
         <p class="text-xl" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, distinctio ut ullam nisi, eos eum mollitia incidunt ratione soluta quaerat debitis eveniet quas quae doloribus earum dolorem quia id quos.</p>
         <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium amet rem accusamus nobis dolorum vitae soluta similique repudiandae odio beatae. Veniam recusandae est quo aut provident dignissimos, saepe adipisci in!</p>
         <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, ipsa ut? Veritatis repellendus iste odit ut, consectetur sed nam commodi cupiditate corrupti eaque? Officiis quod voluptates enim quasi molestiae laborum?</p>
       </div>
-      <div class="p-6">
+      <div class="p-6 animate-fade-left">
         <img class="w-full" src="/images/home/CodeWare.png" alt="Dispositivos" title="dispositivos">
   
       </div>
--- components/FloatWhatsapp.vue
+++ components/FloatWhatsapp.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute bottom-10 right-5 hover:scale-110 transition " >
+  <div class="fixed bottom-10 right-5 hover:scale-110 transition " >
     <div class="rounded-full bg-green-500 p-1.5" >
       <a href="https://api.whatsapp.com/send/?phone=%2B56991595727&text=hola&type=phone_number&app_absent=0">
         <img src="/images/icons/whatsapp.svg" alt="whatsapp" title="whatsapp" class="w-10" >
